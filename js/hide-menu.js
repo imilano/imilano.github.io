baade7408b61a28e3e9296e7a58eb39fef6920f8
imilano <imilano@users.noreply.github.com>
2023-03-21 09:19:45 +0000

    deploy: d97ebf6aa0c617b98b06fd98a1bcad157b88295a

---
+++ js/hide-menu.js
@@ -0,0 +1,53 @@
+(function () {
+	var doc = document.documentElement;
+	var w = window;
+
+	var prevScroll = w.scrollY || doc.scrollTop;
+	var curScroll;
+	var direction = 0;
+	var prevDirection = 0;
+
+
+	// var header = document.getElementsByClassName("masthead")[0];
+	var header = document.getElementById("masthead");
+	var checkScroll = function () {
+
+	  /*
+	  ** Find the direction of scroll
+	  ** 0 - initial, 1 - up, 2 - down
+	  */
+
+	  curScroll = w.scrollY || doc.scrollTop;
+	  if (curScroll > prevScroll) {
+		//scrolled up
+		direction = 2;
+	  }
+	  else if (curScroll < prevScroll) {
+		//scrolled down
+		direction = 1;
+	  }
+
+	  if (direction !== prevDirection && Math.abs(curScroll - prevScroll) > 1) {
+		console.log(`predirection ${prevDirection}, current direction ${direction}, current scroll ${curScroll}`);
+		toggleHeader(direction, curScroll);
+	  }
+
+	  prevScroll = curScroll;
+	};
+
+	var toggleHeader = function (direction, curScroll) {
+	  if (direction === 2 && curScroll > 50) {
+
+		//replace 52 with the height of your header in px
+
+		header.classList.add('hide');
+		prevDirection = direction;
+	  }
+	  else if (direction === 1) {
+		header.classList.remove('hide');
+		prevDirection = direction;
+	  }
+	};
+
+	window.addEventListener('scroll', checkScroll);
+  })();
